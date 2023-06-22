24c4ec41bfcd4ec641b9240e95e2235f421624d1
alperencngz <cengiz.ozturk@ozu.edu.tr>
2023-06-22 09:38:30 +0300

    Added my main NavBar
    Next I will add EventDetailPage's to my events that I will create in EventsPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,13 +27,20 @@ import EventsPage from "./pages/EventsPage";
 import EventDetailPage from "./pages/EventDetailPage";
 import NewEventPage from "./pages/NewEventPage";
 import EditEventPage from "./pages/EditEventPage";
+import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/events", element: <EventsPage /> },
-  { path: "/events/:eventId", element: <EventDetailPage/>},
-  { path: "/events/new", element: <NewEventPage/>},
-  { path: "/events/:eventId/edit", element: <EditEventPage/>}
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "/events", element: <EventsPage /> },
+      { path: "/events/:eventId", element: <EventDetailPage /> },
+      { path: "/events/new", element: <NewEventPage /> },
+      { path: "/events/:eventId/edit", element: <EditEventPage /> }
+    ]
+  },
 ])
 
 function App() {
