3fd9f53f44f94f1004fc351577eacad64a70cc23
alperencngz <cengiz.ozturk@ozu.edu.tr>
2023-06-25 09:03:59 +0300

    Some notes about loaders
    Little side notes to be aware of

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -3,7 +3,9 @@ import { useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-    const events = useLoaderData();
+
+    const response = useLoaderData();
+    const events = response.events;
 
     return (
         <>
@@ -15,12 +17,14 @@ function EventsPage() {
 export default EventsPage;
 
 export async function loader() {
+    /* we cant use react hooks in loader functions but we can use everything else
+    in javascript and default browser setting in loaders */
+    
     const response = await fetch("http://localhost:8080/events");
 
     if (!response.ok) {
         // I will look at this later
     } else {
-        const responseData = await response.json();
-        return responseData.events;
+        return response;
     }
 }
--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -3,10 +3,16 @@ import { Outlet } from "react-router-dom"
 import MainNavigation from "../components/MainNavigation"
 
 function RootLayout() {
+
+    /* this part can be used to let user know something is loading when they swithch to another
+    page and in that page we are using loader function so data is fetched before page opens */
+    // const navigation = useNavigation();
+
     return (
         <>
             <MainNavigation />
             <main>
+                {/* {navigation.state === "loading" && <p>Loading...</p>} */}
                 <Outlet />
             </main>
         </>
