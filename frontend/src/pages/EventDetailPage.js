541ffc479e0a19500a81b9e985dceccbdefa8053
alperencngz <cengiz.ozturk@ozu.edu.tr>
2023-06-27 10:48:02 +0300

    Adding all events below EventDetailPage for every event, and doing the loading of the all events with defer
    Benefit of doing this is we load the Details of single event first which will be faster and after that we load the rest of the events below. We have a problem for now, but I will solve it next commit and finish the project.

--- frontend/src/pages/EventDetailPage.js
+++ frontend/src/pages/EventDetailPage.js
@@ -1,21 +1,32 @@
-import { useRouteLoaderData, json, redirect } from "react-router-dom";
+import { useRouteLoaderData, json, redirect, defer, Await } from "react-router-dom";
 
 import EventItem from "../components/EventItem";
+import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 function EventDetailPage() {
 
-    const data = useRouteLoaderData("event-detail");
+    const { event, events } = useRouteLoaderData("event-detail");
 
     return (
-        <EventItem event={data.event} />
+        <>
+            <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+                <Await resolve={event}>
+                    {loadedEvent => <EventItem event={loadedEvent} />}
+                </Await>
+            </Suspense>
+            <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+                <Await resolve={events}>
+                    {loadedEvents => <EventsList event={loadedEvents} />}
+                </Await>
+            </Suspense>
+        </>
     )
 }
 
 export default EventDetailPage;
 
-export async function loader({ request, params }) {
-
-    const id = params.eventId;
+async function loadEvent(id) {
 
     const response = await fetch("http://localhost:8080/events/" + id);
 
@@ -24,11 +35,49 @@ export async function loader({ request, params }) {
             status: 500,
         })
     } else {
-        return response;
+        const resData = await response.json();
+        return resData.event;
     }
 }
 
-export async function action({params, request}) {
+async function loadEvents() {
+    /* we cant use react hooks in loader functions but we can use everything else
+    in javascript and default browser setting in loaders */
+
+    const response = await fetch("http://localhost:8080/events");
+
+    if (!response.ok) {
+        // return { isError: true, message: "Could not fetch events." };
+        // throw { message : "We could throw error this way too"}
+
+        // throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+        //     status: 500,
+        // });
+
+        throw json(
+            { message: "Could not fetch events." },
+            {
+                status: 500,
+            }
+        )
+
+    } else {
+        const resData = await response.json();
+        return resData.events;
+    }
+}
+
+export async function loader({ request, params }) {
+
+    const id = params.eventId;
+
+    return defer({
+        event: await loadEvent(id),
+        events: loadEvents()
+    })
+}
+
+export async function action({ params, request }) {
 
     const eventId = params.eventId;
 
